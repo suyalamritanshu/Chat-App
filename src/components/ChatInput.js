12f00ac5a18f1ed35d996e2eb400b4da7f086e9c
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-11-24 23:25:56 +0530

    All chats are storing on local storage

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -2,51 +2,72 @@ import React from "react";
 import micIcon from "../assets/mic.svg";
 import sendIcon from "../assets/send.svg";
 import link from "../assets/link.png";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { AuthContext } from "./context/AuthContext";
 
 export default function ChatInput({ message, setMessage, pushMessage }) {
+  const [Data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
   function handleKeyDown(e) {
     if (e.key === "Enter" && message) {
+      setDatatoStorage();
+      getDataFromStorage();
+
       pushMessage();
     }
-    
   }
   const [file, setFile] = useState(null);
+  function setDatatoStorage() {
+    try {
+      setLoading(true);
+      localStorage.setItem("data", message);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  function getDataFromStorage() {
+    try {
+      localStorage.getItem("data");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // useEffect(() => {
+
+  // }, []);
 
   const submitHandler = async (e) => {
-      e.preventDefault();
-      if (file) {
-        const data = new FormData();
-        const fileName = Date.now() + file.name;
-        data.append("name", fileName);
-        data.append("file", file);
-        try {
-          await axios.post("/upload", data);
-        } catch (err) {}
-      }
+    e.preventDefault();
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+  };
 
-    };
   return (
     <div className="chat-input-box">
       <div className="icon emoji-selector">
-      <form className="shareBottom" onSubmit={submitHandler}>
+        <form className="shareBottom" onSubmit={submitHandler}>
           <div className="shareOptions">
-            
-          <label htmlFor="file" className="shareOption">
-         <img src={link} height="20px" width="20px" />
-          <input
-            style={{ display: "none" }}
-            type="file"
-            id="file"
-            accept=".png,.jpeg,.jpg"
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-        </label>
-        </div>
-            </form>
-       
+            <label htmlFor="file" className="shareOption">
+              <img src={link} height="20px" width="20px" />
+              <input
+                style={{ display: "none" }}
+                type="file"
+                id="file"
+                accept=".png,.jpeg,.jpg"
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </label>
+          </div>
+        </form>
       </div>
 
       <div className="chat-input">
@@ -57,7 +78,6 @@ export default function ChatInput({ message, setMessage, pushMessage }) {
           onChange={(e) => setMessage(e.target.value)}
           onKeyDown={handleKeyDown}
         />
-        
       </div>
 
       <div className="icon send" onClick={pushMessage}>
